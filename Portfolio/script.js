29602dc38224adb82500f7d517b42489c4d45d05
Quentin MAMMA <73184884+CyberTorii@users.noreply.github.com>
2024-01-10 12:14:01 +0100

    Add files via upload

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,19 +1,29 @@
 const moreToolsBtn = document.querySelector("#moreToolsBtn");
-moreToolsBtn.addEventListener("click", showMorSoftwares);
+moreToolsBtn.addEventListener("click", showMoreTools);
 
 const moreTools = document.querySelector("#more-tools"); 
 moreTools.style.display = "none";
 
-const moreLangBtn = document.querySelector("#moreLangBtn");
-moreLangBtn.addEventListener("click", showMorTools);
-
-const moreLang = document.querySelector("#more-lang"); 
-moreLang.style.display = "none";
-
-function showMorSoftwares() {
+function showMoreTools() {
     moreTools.style.display = moreTools.style.display === "none" ? "block" : "none";
 }
 
-function showMorTools() {
+const moreLangBtn = document.querySelector("#moreLangBtn");
+moreLangBtn.addEventListener("click", showMoreLang);
+
+const moreLang = document.querySelector("#more-lang");
+moreLang.style.display = "none";
+
+function showMoreLang() {
     moreLang.style.display = moreLang.style.display === "none" ? "block" : "none";
+}
+
+const moreSoftwaresBtn = document.querySelector("#moreSoftwaresBtn");
+moreSoftwaresBtn.addEventListener("click", showMoreSoftwares);
+
+const moreSoftwares = document.querySelector("#more-softwares"); 
+moreSoftwares.style.display = "none";
+
+function showMoreSoftwares() {
+    moreSoftwares.style.display = moreSoftwares.style.display === "none" ? "block" : "none";
 }
